57c520c8746577f4b82d2496388934a140f9cd56
Nelson Elhage <nelhage@nelhage.com>
2018-04-15 08:49:43 -0700

    Add some primitives

--- src/globals.rs
+++ src/globals.rs
@@ -4,10 +4,25 @@ use types::{Type, TypeEnv};
 use std::rc::Rc;
 
 pub fn global_env() -> Rc<TypeEnv<'static>> {
-    TypeEnv::from_types(&[
-        (names::ident("bool"), bool()),
-        (names::ident("int"), integer()),
-    ])
+    let unary_int = Rc::new(Type::Function(integer(), integer()));
+    let binary_int = Rc::new(Type::Function(integer(), Rc::clone(&unary_int)));
+    TypeEnv::with_bindings(
+        &TypeEnv::from_types(&[
+            (names::ident("bool"), bool()),
+            (names::ident("int"), integer()),
+        ]),
+        &[
+            (names::ident("add"), Rc::clone(&binary_int)),
+            (names::ident("sub"), Rc::clone(&binary_int)),
+            (names::ident("mul"), Rc::clone(&binary_int)),
+            (names::ident("dec"), Rc::clone(&unary_int)),
+            (
+                names::ident("iszero"),
+                Rc::new(Type::Function(integer(), bool())),
+            ),
+            (names::ident("not"), Rc::new(Type::Function(bool(), bool()))),
+        ],
+    )
 }
 
 pub fn bool() -> Rc<Type<'static>> {
